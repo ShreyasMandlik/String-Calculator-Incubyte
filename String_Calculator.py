fb28d6eabf124bb65f4889b4f6a862f95966dc6d
Shreyas <mandlikshreyas01@gmail.com>
2022-09-08 17:46:28 +0530

    Unknown numbers replaced by unicode of alphabet

--- String_Calculator.py
+++ String_Calculator.py
@@ -1,38 +1,37 @@
 import re
-from unittest import result
+
+
 def calculator(s):
 
-   ## sum variable for storing the addition value
-   sum=0   
-  
-   ## flag variable for detecting negative value
+    # sum variable for storing the addition value
+    sum = 0
 
-   flag=0
+    # flag variable for detecting negative value
 
-   ## Message for Negative Number   
-   msg="Negative Not allowed : "
+    flag = 0
 
-   ## Adding the number corresponding to the alphabet 
-   for i in s:                     
-        if (96<ord(i)<123):  
-            sum+=(ord(i)-96)
+    # Message for Negative Number
+    msg = "Negative Not allowed : "
 
-   ## find the digit in string and typecasting it into integer and storing it in list using regex 
-   result = [int(d) for d in re.findall(r'-?\d+', s)]    
-   
+    # Adding the number corresponding to the alphabet
+    for i in s:
+        if (ord('a') <= ord(i) <= ord('z')):
+            sum += (ord(i)-ord('a')+1)
 
-   ## Adding values from the list
-   for i in result:
-        if i<0:
-            flag=1
-            msg=msg+str(i)+" " 
-        elif i>=1000:               ##  if number greater than 1000 ignored it 
+    # find the digit in string and typecasting it into integer and storing it in list using regex
+    result = [int(d) for d in re.findall(r'-?\d+', s)]
+
+    # Adding values from the list
+    for i in result:
+        if i < 0:
+            flag = 1
+            msg = msg+str(i)+" "
+        elif i >= 1000:  # if number greater than 1000 ignored it
             pass
         else:
-            sum+=i
+            sum += i
 
-   if flag==0:
-        return(sum)
-   else :
-        return(msg)
-
+    if flag == 0:
+        return (sum)
+    else:
+        return (msg)
