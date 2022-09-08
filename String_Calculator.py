1178cf40d2d78c7282d5b7d3185eb3c35be09907
Shreyas <mandlikshreyas01@gmail.com>
2022-09-08 18:29:20 +0530

    Extra if_else statments removed

--- String_Calculator.py
+++ String_Calculator.py
@@ -1,46 +1,39 @@
 import re
-from string import digits
-
-
-def calculator(s):
+def calculator(input_string):
     sum = 0
     multiply = 1
     detect_negative = 0
     sum_or_multipy = 0
 
-    if len(s) == 0:
+    if len(input_string) == 0:
         return 0
-    first_char = s[0]
+    first_char = input_string[0]
 
     if first_char == '*':
         sum_or_multipy = 1
 
     msg = "Negative Not allowed : "
+    digits = [int(d) for d in re.findall(r'-?\d+', input_string)]
 
-    for i in s:
-        if (ord('a') <= ord(i) <= ord('z')) and sum_or_multipy == 0:
-            sum += (ord(i)-ord('a')+1)
-
-        elif (ord('a') <= ord(i) <= ord('z')) and sum_or_multipy == 1:
-            multiply *= (ord(i)-ord('a')+1)
-
-    digits = [int(d) for d in re.findall(r'-?\d+', s)]
+    for i in input_string:
+        if (ord('a') <= ord(i) <= ord('z')):
+            digits.append((ord(i)-ord('a')+1))
 
     for i in digits:
-        if i < 0 and sum_or_multipy == 0:
+        if i < 0 :
             detect_negative = 1
             msg = msg+str(i)+" "
         elif i >= 1000:
             pass
-        else:
-            if sum_or_multipy == 0:
-                sum += i
-            elif sum_or_multipy == 1:
-                multiply *= i
-
-    if detect_negative == 0 and sum_or_multipy == 0:
-        return (sum)
-    elif detect_negative == 0 and sum_or_multipy == 1:
-        return (multiply)
-    elif detect_negative == 1:
+        elif sum_or_multipy == 0:
+            sum += i
+        elif sum_or_multipy == 1:
+            multiply *= i
+    
+    if detect_negative == 1:
         return (msg)
+    elif sum_or_multipy == 0:
+        return (sum)
+    elif sum_or_multipy == 1:
+        return (multiply)
+    
